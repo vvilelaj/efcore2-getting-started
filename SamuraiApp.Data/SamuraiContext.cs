85aef0655cdc3c875fb5587baf02b0b9bdeb2533
Victor Vilela <vilela_victor@hotmail.com>
2018-06-14 23:10:39 -0500

    Add SamuraiContext class

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samuai> Samuais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Batle> Batles { get; set; }
+    }
+}
