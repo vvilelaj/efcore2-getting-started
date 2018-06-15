f8e19d6c6d407878f3b8fcc74e78836d10bb7fcd
Victor Vilela <vilela_victor@hotmail.com>
2018-06-14 22:56:00 -0500

    Initial solution

---
+++ SamuraiApp.Domain/Batle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class Batle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StarDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Samuai> Samuais { get; set; }
+    }
+}
---
+++ SamuraiApp.Domain/Samuai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class Samuai
+    {
+        public Samuai()
+        {
+            Quogtes = new List<Quote>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quogtes { get; set; }
+        public int BatleId { get; set; }
+    }
+}
