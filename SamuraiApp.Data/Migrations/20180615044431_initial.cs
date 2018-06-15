ee5f8befb04b60f37e7e608fcaef16d5d515d274
Victor Vilela <vilela_victor@hotmail.com>
2018-06-14 23:45:44 -0500

    add migratios to SamuraiApp.Data

---
+++ SamuraiApp.Data/Migrations/20180615044431_initial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    StarDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Samuais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    BatleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samuais", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Samuais_Batles_BatleId",
+                        column: x => x.BatleId,
+                        principalTable: "Batles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    SamuraiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Samuais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samuais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_SamuraiId",
+                table: "Quotes",
+                column: "SamuraiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Samuais_BatleId",
+                table: "Samuais",
+                column: "BatleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "Samuais");
+
+            migrationBuilder.DropTable(
+                name: "Batles");
+        }
+    }
+}
