6a2b04c415ca224deb88f1be1486a0a84c3718ae
Victor Vilela <vilela_victor@hotmail.com>
2018-06-17 23:44:18 -0500

    add LoggerFactory

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public static LoggerFactory MyLoggerFactory = new LoggerFactory(new[]
+        {
+            new ConsoleLoggerProvider((category, level)
+                => category == DbLoggerCategory.Database.Command.Name &&
+                   level == LogLevel.Information, true)
+        });
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlServer(
                 "Server=(localdb)\\mssqllocaldb; Database=SamuraiApp; Trusted_Connection = True;");
         }
 
